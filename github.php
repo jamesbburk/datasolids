99b4a96c4038f781854b0200c4574dbbebba508a
Jamie Burkart <jamie@kc.rr.com>
2011-12-18 23:45:30 -0500

    tracing problem

--- github.php
+++ github.php
@@ -1,9 +1,16 @@
 <?php
+echo "start";
+echo "1";
 `/usr/local/bin/git pull`;
+echo "2";
+`git pull`;
+echo "3";
 $output = shell_exec('/usr/local/bin/git pull');
 var_dump($output);
+echo "4";
 $output = shell_exec('git pull');
 var_dump($output);
+echo "5";
 $output = shell_exec('ls');
 echo "<pre>$output</pre>";
 
