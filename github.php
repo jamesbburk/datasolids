a1cc376d52f5344fec11ef16a4513449dd6d648c
Jamie Burkart <jamie@kc.rr.com>
2011-12-19 17:14:35 -0500

    Recloned onto Server

--- github.php
+++ github.php
@@ -1,15 +1,4 @@
 <?php
-    $output = shell_exec('ls -l /usr/local/bin');
-    var_dump($output);
-    $output = shell_exec('/usr/local/bin/git status');
-    var_dump($output);
-    $output = shell_exec('/usr/local/bin/git pull');
-    var_dump($output);
-    $output = shell_exec('ls -l ../');
-    var_dump($output);
-
-    $output = shell_exec('whoami');
-    var_dump($output);
-    $output = shell_exec('pwd');
+    $output = shell_exec('git pull');
     var_dump($output);
 ?>
