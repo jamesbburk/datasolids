2dc24168d319291ead6c4a6964129612d3a26afe
Jamie Burkart <jamie@kc.rr.com>
2011-12-18 23:43:58 -0500

    github.php

--- github.php
+++ github.php
@@ -2,7 +2,9 @@
 `/usr/local/bin/git pull`;
 $output = shell_exec('/usr/local/bin/git pull');
 var_dump($output);
-$output .= shell_exec('ls');
+$output = shell_exec('git pull');
+var_dump($output);
+$output = shell_exec('ls');
 echo "<pre>$output</pre>";
 
 //phpinfo();
