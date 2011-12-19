3257b8e592f77a8d8a8bdc767024a21db40ef67e
Jamie Burkart <jamie@kc.rr.com>
2011-12-18 23:20:57 -0500

    relative path in github.php

--- github.php
+++ github.php
@@ -1,6 +1,7 @@
 <?php
-$output = shell_exec('git pull');
+$output = shell_exec('/usr/local/bin/git pull');
 $output .= shell_exec('ls');
+$output .= shell_exec('which git');
 echo "<pre>$output</pre>";
 
 //phpinfo();
